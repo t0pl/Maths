b65a2f9f02172884d394d03bf2953220a305eed6
t0pl <37243285+t0pl@users.noreply.github.com>
2019-12-21 15:57:15 +0100

    Add files via upload

---
+++ sequences.py
@@ -0,0 +1,19 @@
+u = {} # {0:1, 1:2, 2:5, 3:26,...}
+
+
+def recurrence_sequence(first_n: int, first_u_n: int, n_seeked: int):
+    """
+    Parameters: first_n corresponds to the value of the initial term of the sequence whose image is known by Un.
+                first_u_n is the image of this initial term by Un
+                n_seeked is the value n searched for
+    Example: U0 = 1
+              0 would then be the value of the "first_n" parameter, and
+              1 the value of "first_u_n".
+    """
+    for n in range(first_n, n_seeked+1):
+        u[n] = u[n-1]*2 - 3 if n-1 in u.keys() else first_u_n
+        yield f"U{n} = {u[n]}"
+        
+
+for i in recurrence_sequence(0, 1, int(input("n value seeked: "))):
+    print(i)
